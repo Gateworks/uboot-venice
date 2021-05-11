b29cdf1dea4d07d89708da66d9daa5117d53e0d4
Tim Harvey <tharvey@gateworks.com>
2021-05-10 17:00:59 -0700

    board: gateworks: venice: gsc: fix voltage offset
    The voltage offset property is in microvolts so must be scaled
    accordingly.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- board/gateworks/venice/gsc.c
+++ board/gateworks/venice/gsc.c
@@ -311,7 +311,7 @@ int gsc_hwmon(void)
 				}
 
 				/* adjust by offset */
-				val += offset;
+				val += (offset / 1000);
 
 				printf("%-8s: %d.%03dV\n", label, val / 1000, val % 1000);
 				break;
