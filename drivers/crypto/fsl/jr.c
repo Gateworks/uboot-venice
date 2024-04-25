7750f3ce45fefee9c01f1407f16d372035519d7b
Marek Vasut <marex@denx.de>
2024-04-26 01:02:07 +0200

    rng: Introduce SPL_DM_RNG
    Add SPL variant of DM_RNG so that the DM_RNG can be disabled in SPL
    if necessary. This may be necessary due to e.g. size constraints of
    the SPL.
    
    Signed-off-by: Marek Vasut <marex@denx.de>

--- drivers/crypto/fsl/jr.c
+++ drivers/crypto/fsl/jr.c
@@ -768,7 +768,7 @@ init:
 	}
 #if CONFIG_IS_ENABLED(OF_CONTROL)
 	if (ofnode_valid(scu_node)) {
-		if (IS_ENABLED(CONFIG_DM_RNG)) {
+		if (CONFIG_IS_ENABLED(DM_RNG)) {
 			ret = device_bind_driver(NULL, "caam-rng", "caam-rng", NULL);
 			if (ret)
 				printf("Couldn't bind rng driver (%d)\n", ret);
@@ -791,7 +791,7 @@ init:
 			return -1;
 		}
 
-		if (IS_ENABLED(CONFIG_DM_RNG)) {
+		if (CONFIG_IS_ENABLED(DM_RNG)) {
 			ret = device_bind_driver(NULL, "caam-rng", "caam-rng",
 						 NULL);
 			if (ret)
