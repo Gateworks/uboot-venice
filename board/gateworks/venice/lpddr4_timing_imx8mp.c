09150eb618c7571d2b99068b7c79e8785f457a30
Tim Harvey <tharvey@gateworks.com>
2025-02-04 18:08:26 -0800

    venice: lpddr4_timing_imx8mm: add 4gb single die support
    Add dram support for the MT53E1G32D2FW-046 RevC part which is a single die
    32Gbit density part vs RevA/B which were dual-die parts.
    
    Use a previously unused EEPROM byte to denote the base configuration variant
    patching the dram configuration as necessary and adding a dram
    description string.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- board/gateworks/venice/lpddr4_timing_imx8mp.c
+++ board/gateworks/venice/lpddr4_timing_imx8mp.c
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0+
 
 #include <linux/kernel.h>
+#include <string.h>
 #include <asm/arch/ddr.h>
+
+#include "eeprom.h"
 
 /*
  * Generated code from MX8M_DDR_tool v3.30 using MX8M_Plus RPAv7
@@ -2380,24 +2383,34 @@ static struct dram_timing_info dram_timing_4gb_dual_die = {
 
 extern struct dram_timing_info dram_timing_4gb_dual_die_1200mhz_16b;
 
-struct dram_timing_info *spl_dram_init(const char *model, int sizemb)
+struct dram_timing_info *spl_dram_init(const char *model, struct venice_board_info *info,
+				       char *dram_desc, size_t sz_desc)
 {
 	struct dram_timing_info *dram_timing;
+	int sizemb = (16 << info->sdram_size);
 
 	switch (sizemb) {
 	case 1024:
 		dram_timing = &dram_timing_1gb_single_die;
+		if (dram_desc)
+			strlcpy(dram_desc, "single-die", sz_desc);
 		break;
 	case 2048:
 		dram_timing = &dram_timing_4gb_dual_die_1200mhz_16b;
+		if (dram_desc)
+			strlcpy(dram_desc, "16b dual-die", sz_desc);
 		break;
 	case 4096:
 		dram_timing = &dram_timing_4gb_dual_die;
+		if (dram_desc)
+			strlcpy(dram_desc, "dual-die", sz_desc);
 		break;
 	default:
 		printf("unsupported");
 		dram_timing = &dram_timing_4gb_dual_die;
 	}
+	if (ddr_init(dram_timing))
+		return NULL;
 
 	return dram_timing;
 }
